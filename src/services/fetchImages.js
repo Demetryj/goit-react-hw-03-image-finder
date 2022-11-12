4bfaec4911293dd52f4a52218f5c667f14d30820
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-11-13 00:43:30 +0300

    add axios

---
+++ src/services/fetchImages.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+export const fetchImages = async (query, page) => {
+  const response = await axios.get('https://pixabay.com/api/', {
+    params: {
+      q: `${query}`,
+      page: `${page}`,
+      key: '30262490-03bcd09aff61aef6d7939f62f',
+      image_type: 'photo',
+      orientation: 'horizontal',
+      per_page: 12,
+    },
+  });
+  const { hits } = response.data;
+  return hits;
+};
