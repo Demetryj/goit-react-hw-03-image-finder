71be6ab3ce079c876ec78847331d76d2e86bdf4a
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-11-13 00:44:14 +0300

    create component Searchbar

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,20 @@
+export const Searchbar = ({ handleSubmit }) => {
+  return (
+    <header className="searchbar">
+      <form className="form" onSubmit={handleSubmit}>
+        <button type="submit" className="button">
+          <span className="button-label">Search</span>
+        </button>
+
+        <input
+          className="input"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          name="query"
+        />
+      </form>
+    </header>
+  );
+};
