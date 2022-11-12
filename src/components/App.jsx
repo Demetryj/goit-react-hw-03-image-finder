6c46764a78d8472b239d4eaf5103909b84b131cc
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-11-13 00:47:09 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,64 @@
-import Component from 'react';
+import { Component } from 'react';
+
+import { Searchbar } from './Searchbar';
+import { ImageGallery } from './ImageGallery';
+import { fetchImages } from '../services/fetchImages';
+import { Button } from './Button';
 
 class App extends Component {
-  rennder() {
-    return;
+  state = {
+    searchQuery: '',
+    page: 1,
+    images: [],
+    error: null,
+    loaded: false,
+  };
+
+  async componentDidUpdate(_, prevState) {
+    const { searchQuery, page, images, loaded } = this.state;
+
+    if (prevState.searchQuery !== searchQuery || prevState.page !== page)
+      try {
+        this.setState({ loaded: true });
+
+        const searchImages = await fetchImages(searchQuery, page);
+        this.setState(prevState => {
+          return {
+            images: [...prevState.images, ...searchImages],
+          };
+        });
+      } catch (error) {
+        console.log(error.message);
+      } finally {
+        this.setState({ loaded: false });
+      }
+  }
+
+  getSearchQuery = event => {
+    event.preventDefault();
+    const inputValue = event.target.elements.query.value.trim().toLowerCase();
+
+    this.setState({ searchQuery: inputValue, page: 1, images: [] });
+  };
+
+  onLoadMoreImages = () => {
+    const { page } = this.state;
+
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+  };
+
+  render() {
+    const { searchQuery, images, loaded } = this.state;
+    return (
+      <>
+        <Searchbar handleSubmit={this.getSearchQuery} />
+        {loaded ? <p>LOADING...</p> : null}
+        {searchQuery && <ImageGallery imageItems={images} />}
+        {images.length > 0 && !loaded && (
+          <Button handleClickLoadMore={this.onLoadMoreImages}>Load more</Button>
+        )}
+      </>
+    );
   }
 }
 
