4a4d48710a4e52ea53be3d774e2d7ef34a34111e
Demetryj <103371743+Demetryj@users.noreply.github.com>
2022-11-14 14:42:43 +0300

    add handleClick on button

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,7 +1,15 @@
+import PropTypes from 'prop-types';
+import css from './button.module.css';
+
 export const Button = ({ handleClickLoadMore, children }) => {
   return (
-    <button type="button" onClick={handleClickLoadMore}>
+    <button type="button" className={css.button} onClick={handleClickLoadMore}>
       {children}
     </button>
   );
+};
+
+Button.propTypes = {
+  handleClickLoadMore: PropTypes.func.isRequired,
+  children: PropTypes.string,
 };
